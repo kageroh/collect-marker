c6705f7125bea51035703fa200df6c9b692d8b99
kageroh <masao.yokoyama@gmail.com>
2010-06-08 18:00:24 +0900

    wscにしてみた

--- collect-marker.bat
+++ collect-marker.bat
@@ -1,8 +1,15 @@
 @echo off
 setlocal
-set OUTPUT=analyze.log
+
+rem regsvr32 /s collect-marker.wsc
+
+rem set OUTPUT=analyze.log
+set OUTPUT=debug.log
+
 cscript //Nologo header.wsf > %OUTPUT%
+
 rem for %%i in (..\txt\*.txt) do cscript //Nologo collect-marker.wsf "%%i" utf-8 >> %OUTPUT%
 for %%i in (*.txt) do cscript //Nologo collect-marker.wsf "%%i" utf-8 >> %OUTPUT%
+
 endlocal
 echo on
